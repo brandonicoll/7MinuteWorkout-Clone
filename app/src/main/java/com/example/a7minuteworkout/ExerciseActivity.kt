83e033dd9b8096f09c8d06809456405b928b5c77
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-05 21:37:04 -0400

    Copied and modified activity_exercise.xml linear layout to start creating exercise timer.

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.example.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
 
