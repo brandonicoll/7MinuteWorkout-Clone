8ce600f48a788ee2df82cc7e64e2da3b91f9c85e
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-07 13:07:13 -0400

    Implemented functionality for custom back dialog -> once back is pressed on actionbar.

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minuteworkout
 
+import android.app.Dialog
 import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_exercise.*
+import kotlinx.android.synthetic.main.dialog_custom_back_confirmation.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -40,7 +42,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             actionbar.setDisplayHomeAsUpEnabled(true)
         }
         toolbar_exercise_activity.setNavigationOnClickListener {
-            onBackPressed()
+            customDialogForBackButton() //run custom dialog that will handle back functionality
         }
 
         tts = TextToSpeech(this, this)
@@ -180,6 +182,20 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 Log.e("TTS", "Initialization failed.")
             }
         }
+    }
+
+    private fun customDialogForBackButton() {
+        val customDialog = Dialog(this)
+
+        customDialog.setContentView(R.layout.dialog_custom_back_confirmation) //the custom dialog we just made - set the xml as the view of it
+        customDialog.tvYes.setOnClickListener {
+            finish()
+            customDialog.dismiss()
+        }
+        customDialog.tvNo.setOnClickListener {
+            customDialog.dismiss()
+        }
+        customDialog.show()
     }
 
 }
