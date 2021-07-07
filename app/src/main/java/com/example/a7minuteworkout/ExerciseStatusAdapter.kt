e03c5ad5f0cbf14c60aaee67b97a4fc435a7b4ff
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-06 23:07:12 -0400

    Completed Adapter functions -> needs implementation in ExerciseActivity.kt now.

--- app/src/main/java/com/example/a7minuteworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseStatusAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.a7minuteworkout
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -12,16 +13,18 @@ class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>, val context: Co
         val tvItem = view.tvItem
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder { //inflate the custom layout
+        return ViewHolder(LayoutInflater.from(context)
+            .inflate(R.layout.item_exercise_status, parent, false))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) { //assign value to each text
+        val model: ExerciseModel = items[position]
+        holder.tvItem.text = model.getId().toString() //getid is a func i declared in my exercise model.kt
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int { //returns the number of items in list
+        return items.size
     }
 
 }
